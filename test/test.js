31d2f0df577f69f78eb5d844e5a9a0243956b58b
Tuyen Cao Huy <Tuyen.H.Cao@carlsberg.asia>
2020-03-10 13:26:37 +0700

    refactor codes

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
+var request = require('supertest');
+var app = require('../index.js');
+describe('GET /', function() {
+ it('respond with hello world', function(done) {
+ //navigate to root and check the the response is "hello world"
+ request(app).get('/').expect('Hello World!', done);
+ });
 });
