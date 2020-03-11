5146b0e3eda68fb507a4f00d9f9c041d18f3365e
Cao Huy Tuyen <tuyencaovn@gmail.com>
2020-03-12 02:55:40 +0700

    add misc

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => res.send('Hello Jenkin!'))
 
 app.get('/done', (req, res) => res.send('Done!'))
 
-app.get('/success', (req, res) => res.send('Success!'))
+app.get('/success', (req, res) => res.send('Everything Success!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
  
