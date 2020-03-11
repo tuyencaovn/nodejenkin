40ac7a264bfa6f93f665993d76a74355177835b7
Cao Huy Tuyen <tuyencaovn@gmail.com>
2020-03-11 22:54:21 +0700

    add success uri

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello Jenkin!'))
+
 app.get('/done', (req, res) => res.send('Done!'))
+
+app.get('/success', (req, res) => res.send('Success!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
  
